5a4aced3b9301fc5d1ea0f4eff78c0352ccd4c7c
omegatech-andre <omega.ti_ind@outlook.com>
2025-01-28 16:57:35 -0300

    chore: botões de section agora fazer uma animação no hover

--- src/components/_ui/btnSections/btnSections.tsx
+++ src/components/_ui/btnSections/btnSections.tsx
@@ -1,20 +1,27 @@
 import { btnSectionsMock } from "@/utils/btnSectionsMock";
 import { Card, Flex, Group, Image, Text } from "@mantine/core";
+import { useHover } from "@mantine/hooks";
 
 export default function BtnSections() {
-  const buttons = btnSectionsMock.map((btn, index) => (
-    <Card key={index} component="a" href={btn.link} p={"0"}>
-      <Flex w={"9rem"} h={"7rem"} direction={"column"} justify={"space-between"}>
-        <Flex flex={"1"} direction={"column"} justify={"center"} align={"center"} gap={"xs"}>
-          <Image src={btn.src} w={"40"} alt={btn.alt}/>
-          <Text ta={"center"} fw={"bold"} fz={"sm"} inline>
-            {btn.titulo}
-          </Text>
+  const buttons = btnSectionsMock.map((btn, index) => {
+    const { hovered, ref } = useHover();
+
+    return (
+      <Card key={index} ref={ref} component="a" href={btn.link} p={"0"}>
+        <Flex w={"9rem"} h={"7rem"} direction={"column"} justify={"space-between"}>
+          <Flex flex={"1"} direction={"column"} justify={"center"} align={"center"} gap={"xs"}>
+            <Image src={btn.src} w={"40"} alt={btn.alt} />
+            <Text ta={"center"} fw={"bold"} fz={"sm"} inline>
+              {btn.titulo}
+            </Text>
+          </Flex>
+          <Group bg={btn.cor} h={hovered ? "10" : "7"} w={"100%"} style={{
+            transitionDuration: ".1s"
+          }}></Group>
         </Flex>
-        <Group bg={btn.cor} h={"7"} w={"100%"}></Group>
-      </Flex>
-    </Card>
-  ));
+      </Card>
+    )
+  });
 
   return (
     <>

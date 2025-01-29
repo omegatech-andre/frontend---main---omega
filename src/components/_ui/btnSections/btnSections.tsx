fe37ce3a91dbefbeb63c6a088d97113d5adce45a
omegatech-andre <omega.ti_ind@outlook.com>
2025-01-29 07:49:15 -0300

    chore: adicionando gsap em headerMain e btnSections

--- src/components/_ui/btnSections/btnSections.tsx
+++ src/components/_ui/btnSections/btnSections.tsx
@@ -1,13 +1,38 @@
 import { btnSectionsMock } from "@/utils/btnSectionsMock";
+import { useGSAP } from "@gsap/react";
 import { Card, Flex, Group, Image, Text } from "@mantine/core";
 import { useHover } from "@mantine/hooks";
+import gsap from "gsap";
+import { useRef } from "react";
 
 export default function BtnSections() {
+  const btnSectionRef = useRef(null);
+
+  useGSAP(() => {
+    if (btnSectionRef.current) {
+      gsap
+        .set(".object-animated", {
+          yPercent: 100,
+          opacity: 0,
+        });
+
+      gsap
+        .timeline()
+        .to(".object-animated", {
+          delay: 1,
+          opacity: 1,
+          yPercent: 0,
+          duration: 1.5,
+          stagger: .2
+        });
+    }
+  }, { scope: btnSectionRef })
+
   const buttons = btnSectionsMock.map((btn, index) => {
     const { hovered, ref } = useHover();
 
     return (
-      <Card key={index} ref={ref} component="a" href={btn.link} p={"0"}>
+      <Card className="object-animated" opacity={0} key={index} ref={ref} component="a" href={btn.link} p={"0"}>
         <Flex w={"9rem"} h={"7rem"} direction={"column"} justify={"space-between"}>
           <Flex flex={"1"} direction={"column"} justify={"center"} align={"center"} gap={"xs"}>
             <Image src={btn.src} w={"40"} alt={btn.alt} />
@@ -25,7 +50,7 @@ export default function BtnSections() {
 
   return (
     <>
-      <Flex pos={"relative"} w={"90vw"} m={"auto"} style={{
+      <Flex ref={btnSectionRef} pos={"relative"} w={"90vw"} m={"auto"} style={{
         overflowX: "auto",
       }}>
         <Flex gap={"sm"} m={"auto"}>
--- src/components/_ui/headerMain/headerMain.tsx
+++ src/components/_ui/headerMain/headerMain.tsx
@@ -6,10 +6,31 @@ import { Button, Card, Flex, Image, Stack, Text } from "@mantine/core";
 import Autoplay from "embla-carousel-autoplay";
 import { useRef } from "react";
 import BtnSections from "../btnSections/btnSections";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function HeaderMain() {
   const { isMobile } = themeDevices();
   const autoplay = useRef(Autoplay({ delay: 3000 }));
+  const headerMainRef = useRef(null);
+
+  useGSAP(() => {
+    if (headerMainRef.current) {
+      gsap
+        .set(".object-animated", {
+          yPercent: 100,
+          opacity: 0,
+        });
+
+      gsap
+        .timeline()
+        .to(".object-animated", {
+          opacity: 1,
+          yPercent: 0,
+          duration: .5,
+        });
+    }
+  }, { scope: headerMainRef })
 
   const slides = carouselMockImages.map((url, index) => (
     <Carousel.Slide key={index}>
@@ -21,13 +42,15 @@ export default function HeaderMain() {
 
   return (
     <>
-      <Stack gap={"lg"} bg={"linear-gradient(180deg, #950000 0%, #292626 100%)"} p={"lg"}>
-        <Flex justify={"space-between"} w={"90vw"} m={"0 auto"}>
-          <Button variant="outline" color={"white"} component="a" href="/">Início</Button>
-          <Button variant="outline" color={"white"} component="a" href="/boletim-tecnico">Boletim Téc.</Button>
+      <Stack ref={headerMainRef} gap={"lg"} bg={"linear-gradient(180deg, #950000 0%, #292626 100%)"} p={"lg"}>
+        <Flex component={"span"} style={{ overflow: "hidden" }} justify={"space-between"} w={"90vw"} m={"0 auto"}>
+          <Button className="object-animated" opacity={0} variant="outline" color={"white"} component="a" href="/">Início</Button>
+          <Button className="object-animated" opacity={0} variant="outline" color={"white"} component="a" href="/boletim-tecnico">Boletim Téc.</Button>
         </Flex>
         <Card w={"90vw"} m={"0 auto"} p={0} radius={"sm"} bg={"none"} pos={"relative"}>
           <Carousel
+            className="object-animated"
+            opacity={0}
             loop
             withControls={false}
             height={"auto"}
@@ -38,13 +61,13 @@ export default function HeaderMain() {
             {slides}
           </Carousel>
           <Flex pos={"absolute"} w={"100%"} h={"100%"} justify={"center"} direction={"column"} ta={"center"} align={"center"} p={"xl"} pt={"0"}>
-            <Image src={"./images/logo/logo-01.webp"} w={isMobile ? "12rem" : "20rem"} />
-            <Text fz={isMobile ? "30" : "64"} fw={"bold"} c={"white"} inline style={{
+            <Image className="object-animated" opacity={0} src={"./images/logo/logo-01.webp"} w={isMobile ? "12rem" : "20rem"} />
+            <Text className="object-animated" opacity={0} fz={isMobile ? "30" : "64"} fw={"bold"} c={"white"} inline style={{
               textShadow: "2px 2px #232323"
             }}>
               SOMOS A ÔMEGA
             </Text>
-            <Text fz={isMobile ? "sm" : "md"} fw={"bold"} c={"white"} w={"33rem"} maw={"80vw"} style={{
+            <Text className="object-animated" opacity={0} fz={isMobile ? "sm" : "md"} fw={"bold"} c={"white"} w={"33rem"} maw={"80vw"} style={{
               textShadow: "2px 2px #232323"
             }}>
               A Ômega é uma Indústria especializada em tintas serigráficas, produzindo esse padrão de qualidade desde 2021, tornando-se referência em Caruaru - PE.
